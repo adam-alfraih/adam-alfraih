6546b84c9de9b5cd6bf90a27a407f9ec078c85f5
John Doe <adamalfraih@gmail.com>
2022-08-02 14:26:21 +0200

    add logo

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -34,7 +34,9 @@ export const NavBar = () => {
         <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
         <Container>
             <Navbar.Brand href="#home">
-                <img src={logo} alt="Logo" />
+                <div className="navbar-logo">
+                    <h1>A</h1>
+                </div>
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav">
                 <span className="navbar-toggler-icon"></span>
