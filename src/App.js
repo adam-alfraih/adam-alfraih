248e5d11537e6a619a20d04844c8f473c8786916
John Doe <adamalfraih@gmail.com>
2022-08-02 11:45:24 +0200

    create banner with animations

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import  { NavBar } from './Components/NavBar';
+import { Banner } from './Components/Banner';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
     <NavBar />
+    <Banner />
     </div>
   );
 }
 
 export default App;
+
